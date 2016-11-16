c64d3098f64dddd8312c3a3310bcd53b8319290f
Hébert, François <francois.hebert@elekta.com>
2016-11-16 13:26:58 -0500

    First commit with first Feature file

---
+++ StudentManager/TestAcceptation/ListStudent.feature
@@ -0,0 +1,14 @@
+﻿Feature: ListStudent
+	In order to know student names enrolled in a specific course
+	As a teacher
+	I want to list the student in a course
+
+@mytag
+Scenario Outline: list student - green path
+	Given I have a <Student1> and <Student2> in <class>
+	When I run this command "-f testDB -listStudent --class=<class>"
+	Then I should get on the screen "Michel Jean\nThibodeau Gustave\n"
+	
+	Examples: 
+		| Student1 | Student2  | class  |
+		| Michel   | Thibodeau | CHI001 |
---
+++ StudentManager/TestAcceptation/ListStudentSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace TestAcceptation
+{
+    [Binding]
+    public class ListStudentSteps
+    {
+        [Given(@"I have a (.*) and (.*) in (.*)")]
+        public void GivenIHaveAAndIn(string p0, string p1, string p2)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I run this command ""(.*)""")]
+        public void WhenIRunThisCommand(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should get on the screen ""(.*)""")]
+        public void ThenIShouldGetOnTheScreen(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
