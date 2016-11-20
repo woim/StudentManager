59faee9d75eb9b9c693ca8659b3b7f690feb670e
Hébert, François <francois.hebert@elekta.com>
2016-11-20 15:56:04 -0500

    Creation dataBase skeleton for feature AddClass

---
+++ StudentManager/ClassLibrary/DataBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class DataBase
+    {
+        public DataBase(string archiveName)
+        {
+
+        }
+
+        public void AddClass(string className)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
