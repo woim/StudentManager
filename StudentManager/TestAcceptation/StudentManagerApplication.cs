e5cb9eca32efffcc7eb3ab613c9abc0bdb537085
Hébert, François <francois.hebert@elekta.com>
2016-11-19 23:35:52 -0500

    Interface for calling application from feature steps

---
+++ StudentManager/TestAcceptation/StudentManagerApplication.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using TechTalk.SpecFlow;
+
+namespace TestAcceptation
+{
+    public class StudentManagerApplication
+    {
+        private string m_applicationName;
+        private Process m_application;
+                
+        public StudentManagerApplication()
+        {
+            m_applicationName = "";
+            m_application = new Process();
+            m_application.StartInfo.FileName = m_applicationName;
+        }
+
+        public void AddClass(string className)
+        {
+            string cmd = "-f fakeDB --addCalss=" + className;
+            m_application.StartInfo.Arguments = cmd;
+            m_application.Start();
+        }
+    }
+
+    public static class Application
+    {
+        private const string Key = "application";
+
+        public static StudentManagerApplication Current
+        {
+            get
+            {
+                if (!ScenarioContext.Current.ContainsKey(Key))
+                {
+                    ScenarioContext.Current[Key] = new StudentManagerApplication();
+                }
+                return ScenarioContext.Current[Key] as StudentManagerApplication;
+            }           
+        }
+    }
+}
