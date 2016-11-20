d748dd1b4f7faff52eb6b248ad4ab72769addb89
Hébert, François <francois.hebert@elekta.com>
2016-11-20 15:52:55 -0500

    Refactoring main application for AddClass feature

--- StudentManager/StudentManager/Program.cs
+++ StudentManager/StudentManager/Program.cs
@@ -4,38 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using Mono.Options;
 
+using Mono.Options;
+using ClassLibrary;
 
 namespace StudentManager
 {
-
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello");
-
             // these variables will be set when the command line is parsed
-            var verbosity = 0;
-            var shouldShowHelp = false;
-            var names = new List<string>();
-            var repeat = 1;
+            //var verbosity = 0;
+            var shouldShowHelp = true;
+            string dataBaseName = null;
+            string className = null;
+            //var repeat = 1;
 
             // thses are the available options, not that they set the variables
             var options = new OptionSet {
-                { "n|name=", "the name of someone to greet.", n => names.Add (n) },
-                { "r|repeat=", "the number of times to repeat the greeting.", (int r) => repeat = r },
-                { "v", "increase debug message verbosity", v => {
-                    if (v != null)
-                        ++verbosity;
-                } },
-                { "h|help", "show this message and exit", h => shouldShowHelp = h != null },
+                { "d|dataBase", "the database.", d => dataBaseName = d },
+                { "addClass=", "add class to the database.", addClass => className = addClass },
+                //{ "r|repeat=", "the number of times to repeat the greeting.", (int r) => repeat = r },
+                //{ "v", "increase debug message verbosity", v => {
+                //    if (v != null)
+                //        ++verbosity;
+                //} },
+                { "h|help", "show help message and exit", h => shouldShowHelp = h != null },
             };
 
-            Console.WriteLine("Usage: OptionsSample.exe [OPTIONS]+ message");
-            Console.WriteLine("Greet a list of individuals with an optional message.");
-            Console.WriteLine("If no message is specified, a generic greeting is used.");
+            Console.WriteLine("Usage: StudentManager.exe [OPTIONS]");
+            Console.WriteLine("Manage a school data base.");
             Console.WriteLine();
 
             // output the options
@@ -51,10 +50,18 @@ namespace StudentManager
             catch (OptionException e)
             {
                 // output some error message
-                Console.Write("greet: ");
+                Console.Write("StudentManager: ");
                 Console.WriteLine(e.Message);
-                Console.WriteLine("Try `greet --help' for more information.");
+                Console.WriteLine("Try `StudentManager.exe --help' for more information.");
                 return;
+            }
+
+            // Create the data base and load it with arhive
+            DataBase dataBase = new DataBase(dataBaseName);
+            
+            if (!String.IsNullOrEmpty(className))
+            {
+                dataBase.AddClass(className);
             }
         }
     }
