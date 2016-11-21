ade501173b5d1cc2bceb33e43e0b56eae5bf62d7
Hébert, François <francois.hebert@elekta.com>
2016-11-21 18:55:01 -0500

    Unit test Class pass test add student

--- StudentManager/ClassLibrary/Class.cs
+++ StudentManager/ClassLibrary/Class.cs
@@ -6,17 +6,25 @@ namespace ClassLibrary
     public class Class
     {
         private string className;
+        private List<Student> m_listStudents;
 
         public Class(string className)
         {
             this.className = className;
+            m_listStudents = new List<Student>();
         }
 
-        public List<Student> Students { get; set; }
+        public List<Student> Students
+        {
+            get
+            {
+                return m_listStudents;
+            }
+        }
 
         public void AddStudent(Student student)
         {
-            throw new NotImplementedException();
+            m_listStudents.Add(student);
         }
     }
 }
