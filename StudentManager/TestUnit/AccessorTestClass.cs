59808ca720473c26a9b1f3aa07ca3984e0484fb4
Hébert, François <francois.hebert@elekta.com>
2016-11-21 18:46:11 -0500

    Finishing unit test for Accessor

--- StudentManager/TestUnit/AccessorTestClass.cs
+++ StudentManager/TestUnit/AccessorTestClass.cs
@@ -1,8 +1,9 @@
 ﻿using NUnit.Framework;
-using System;
+//using System;
 using ClassLibrary;
 using System.Collections.Generic;
-
+//using System.Linq;
+using System.IO;
 
 namespace TestUnit
 {
@@ -11,21 +12,44 @@ namespace TestUnit
 	{
         Accessor m_accessor;
         List<Class> m_classesExpected;
+        string m_testDataBaseName;
 
         [SetUp]
         public void createDatabase()
         {
+            m_accessor = new Accessor();
 
-            "MAT008/Loiseau,Martin=12.0,13.0,14.0|Thibodeau,Jean,Charles-Henri=18.0,18.0,18.0";
-            "BIO012/Thibodeau,Jean,Charles-Henri=17.0,13.0,15.0|Loiseau,Martin=17.0,19.0,15.0";
-            System.IO.File.WriteAllLines(m_dataBaseName, data);            
+            string text = "MAT008/Loiseau,Martin=12.0,13.0,14.0|Thibodeau,Jean=18.0,18.0,18.0";
+            m_testDataBaseName = TestContext.CurrentContext.WorkDirectory.ToString() + @"\testableDataBase.txt";
+            File.WriteAllText(m_testDataBaseName, text);
+
+            Student student1 = new Student();
+            student1.Name = "Loiseau";
+            student1.FirstName = "Martin";
+            student1.Notes = new List<double> { 12.0, 13.0, 14.0 };
+
+            Student student2 = new Student();
+            student2.Name = "Thibodeau";
+            student2.FirstName = "Jean";
+            student2.Notes = new List<double> { 18.0, 18.0, 18.0 };
+            
+            Class testableClass = new Class("MAT008");
+            testableClass.Students = new List<Student> { student1, student2 };
+
+            m_classesExpected = new List<Class> { testableClass };
         }
 
 		[Test]
-		public void LoadTestCase()
+		public void ShouldLoadDataBase()
 		{
-            List<Class> classesActual = m_accessor.Load(testDataBaseName);
-            Assert.That(m_classesExpected.SequenceEqual(classesActual));
+            List<Class> classesActual = m_accessor.Load(m_testDataBaseName);
+            Assert.That(classesActual, Is.EquivalentTo(m_classesExpected));
+        }
+        
+        [TearDown]
+        public void DisposeDataBase()
+        {
+            File.Delete(m_testDataBaseName);
         }
 	}
 }
