467cbe9d1da85e2b11abad3dd3b1aab2f96649f1
Hébert, François <francois.hebert@elekta.com>
2016-11-21 18:47:09 -0500

    Creation fo Student class for unit test Accessor

---
+++ StudentManager/ClassLibrary/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public List<double> Notes { get; set; }
+
+        public Student()
+        {
+
+        }
+    }
+}
