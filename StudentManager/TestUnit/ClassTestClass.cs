28400ac951637474314a5622b3b3ee3735e0a399
Hebert <hebert.francois_paul@ens.uqam.ca>
2016-11-24 20:00:43 -0500

    Unit test remove student for class Course

--- StudentManager/TestUnit/ClassTestClass.cs
+++ StudentManager/TestUnit/ClassTestClass.cs
@@ -1,31 +1,34 @@
 ﻿using NUnit.Framework;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
 using ClassLibrary;
 using System.Collections.Generic;
+
 
 namespace TestUnit
 {
     [TestFixture]
     public class ClassTestClass
     {
+        Course m_class;
+        Student m_student;
+        List<Student> m_listStudentExpected;
+
+        [SetUp]
+        public void CreationFakeClass()
+        {
+            m_class = new Course("MAT001");
+            m_student = new Student();
+            m_student.Name = "Robert";
+            m_student.FirstName = "Jean";
+            m_student.Notes = new List<double> { 12.0, 15.0 };
+
+            m_class.AddStudent(m_student);            
+        }
+
         [Test]
         public void ShouldAddAStudent()
         {
-            Course newClass = new Course("MAT001");
-            Student student = new Student();
-            student.Name = "Robert";
-            student.FirstName = "Jean";
-            student.Notes = new List<double> { 12.0, 15.0 };             
-
-            newClass.AddStudent(student);
-            List<Student> listStudentExpected = new List<Student> { student };
-
-            // TODO: Add your test code here
-            Assert.That(newClass.Students, Is.EquivalentTo(listStudentExpected));
+            m_listStudentExpected = new List<Student> { m_student };
+            Assert.That(m_class.Students, Is.EquivalentTo(m_listStudentExpected));
         }
 
         [TestCase("MA")]
@@ -34,6 +37,14 @@ namespace TestUnit
         public void ShouldHaveACorrectFormat(string courseName)
         {
             Assert.That(() => new Course(courseName), Throws.Exception);
+        }
+
+        [Test]
+        public void ShouldRemoveAStudent()
+        {
+            string studentName = m_student.Name;
+            m_class.RemoveStudent(studentName);
+            Assert.That(m_class.Students, Is.Empty);
         }
     }
 }
